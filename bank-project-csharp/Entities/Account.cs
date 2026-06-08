a905840b3f1b1dda2bf145011553c99973bf3afe
tlima1011 <thiagolima1011@gmail.com>
2026-06-08 20:28:32 -0300

    Account Class Created

---
+++ bank-project-csharp/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bank_project_csharp.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; } 
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+
+    }
+}
